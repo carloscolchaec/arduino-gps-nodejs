6e018383d926bcdaa88d990739297483debf6fd7
Carlos Colcha <ccolcha30@gmail.com>
2022-08-31 23:59:51 -0500

    make it better

--- services/dbconnection.js
+++ services/dbconnection.js
@@ -1,17 +1,23 @@
 const mysql = require("mysql");
 
 
-const userDB = "sql5514775";
-const bdDB = "sql5514775";
-const passwDB = "M7prz4qVvK";
-const hostDB = "sql5.freesqldatabase.com";
+// const userDB = "sql5514775"; 
+// const bdDB = "sql5514775";
+// const passwDB = "M7prz4qVvK";
+// const hostDB = "sql5.freesqldatabase.com";
+
+
+const userDB = "sql10516525"; 
+const bdDB = "sql10516525";
+const passwDB = "KF24ZZgj6I";
+const hostDB = "sql10.freemysqlhosting.net";
+
+
 
 // const userDB = "root";
 // const bdDB = "walert_db";
 // const passwDB = "";
 // const hostDB = "localhost";
-
-
 
 // Connection DB
 const conn = mysql.createConnection({
